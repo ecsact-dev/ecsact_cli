1f96dfcdb5c60fa2e2b56f701ac1afe879257ad4
Ezekiel Warren <zekewarren@gmail.com>
2023-06-07 14:28:43 -0700

    feat: build subcommand

---
+++ commands/build.hh
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include <type_traits>
+
+#include "./command.hh"
+
+namespace ecsact::cli::detail {
+
+int build_command(int argc, char* argv[]);
+static_assert(std::is_same_v<command_fn_t, decltype(&build_command)>);
+
+} // namespace ecsact::cli::detail

57d7163b479d06c1a3474e74571d7c861cd23e45
Ezekiel Warren <zekewarren@gmail.com>
2023-09-21 11:18:26 -0700

    chore: formatting

--- test/test_codegen_plugin.cc
+++ test/test_codegen_plugin.cc
@@ -5,13 +5,13 @@
 using namespace std::string_view_literals;
 
 auto ecsact_codegen_plugin_name() -> const char* {
-  return "txt";
+	return "txt";
 }
 
 auto ecsact_codegen_plugin( //
 	ecsact_package_id         package_id,
 	ecsact_codegen_write_fn_t write_fn
 ) -> void {
-  auto test_message = "this is just a test, breathe!"sv;
-  write_fn(test_message.data(), test_message.size());
+	auto test_message = "this is just a test, breathe!"sv;
+	write_fn(test_message.data(), test_message.size());
 }

1c70875bb499aadab9d14374cdab8e77800285fd
Ezekiel Warren <zekewarren@gmail.com>
2023-10-05 13:06:15 -0700

    feat: added extra files to build recipe test

---
+++ test/build_recipe/local_dep.cc
@@ -0,0 +1,7 @@
+#include "local_dep.hh"
+
+#include <iostream>
+
+auto local_dep::example() -> void {
+  std::cout << "hello from local dep example()\n";
+}
---
+++ test/build_recipe/local_dep.hh
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace local_dep {
+  auto example() -> void;  
+}

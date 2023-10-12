9c23d17fe6a3749485f8adcd1f7bc61eae2f5d0e
Ezekiel Warren <zekewarren@gmail.com>
2023-10-11 17:48:08 -0700

    chore: formatting

--- ecsact/cli/detail/argv0.hh
+++ ecsact/cli/detail/argv0.hh
@@ -3,5 +3,5 @@
 #include <filesystem>
 
 namespace ecsact::cli::detail {
-  auto canon_argv0(const char* argv0) -> std::filesystem::path;
+auto canon_argv0(const char* argv0) -> std::filesystem::path;
 }

51eb74aa70e16bd637baede08c64dba425019b2a
Ezekiel Warren <zaucy@users.noreply.github.com>
2023-10-15 19:22:49 -0700

    chore: more methods in build recipe test (#54)

--- test/build_recipe/ecsact_build_test.cc
+++ test/build_recipe/ecsact_build_test.cc
@@ -3,6 +3,12 @@
 
 #include "local_dep.hh"
 
+ecsact_registry_id ecsact_create_registry( //
+	const char*
+) {
+	return ecsact_invalid_registry_id;
+}
+
 ecsact_execute_systems_error ecsact_execute_systems( //
   ecsact_registry_id,
   int,
@@ -13,5 +19,6 @@ ecsact_execute_systems_error ecsact_execute_systems( //
 	// This is an incorrect way to use this function, but this test is only caring
 	// about the import
 	ecsact_system_execution_context_get(nullptr, {}, nullptr);
+	ecsact_system_execution_context_action(nullptr, nullptr);
 	return ECSACT_EXEC_SYS_OK;
 }
--- test/build_recipe/test.nu
+++ test/build_recipe/test.nu
@@ -1,7 +1,8 @@
 let dir = $env.FILE_PWD;
 let ecsact_file = $dir + '/test.ecsact';
-let recipe_file = $dir + '/ecsact_build_test_recipe.yml';
+let recipe_file = $dir + '/example-recipe.yml';
 let output_file = $dir + '/tmp/test';
 let temp_dir = $dir + '/tmp';
 
-bazel run @ecsact_cli -- build $ecsact_file -r $recipe_file -o $output_file --temp_dir $temp_dir -f text
+# bazel run @ecsact_cli -- build $ecsact_file -r $recipe_file -o $output_file --temp_dir $temp_dir -f text
+ecsact build $ecsact_file -r $recipe_file -o $output_file --temp_dir $temp_dir -f text

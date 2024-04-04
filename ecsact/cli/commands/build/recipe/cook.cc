f6dae1023829afbea7eef81ce961d8092a4b52a1
Ezekiel Warren <zaucy@users.noreply.github.com>
2024-04-04 14:37:09 -0700

    fix: intermediate-dir for msvc cl (#80)

--- ecsact/cli/commands/build/recipe/cook.cc
+++ ecsact/cli/commands/build/recipe/cook.cc
@@ -502,6 +502,10 @@ auto cl_compile(compile_options options) -> int {
 	cl_args.push_back("/O2");
 	cl_args.push_back("/GL");
 	cl_args.push_back("/MP");
+	cl_args.push_back("/Fo:"); // typos:disable-line
+	cl_args.push_back(
+		std::format("{}\\", fs::path{options.work_dir}.lexically_normal().string())
+	);
 
 	auto generated_defines =
 		ecsact::cli::cc_defines_gen(options.imports, options.exports);

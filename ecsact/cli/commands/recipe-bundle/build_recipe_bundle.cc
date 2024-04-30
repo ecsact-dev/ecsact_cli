6819a61344be4311d2a2b4553c6d34ee541913d0
Ezekiel Warren <ezekiel@seaube.com>
2024-04-30 13:36:46 -0700

    chore: update libarchive and wip recipe bundle

--- ecsact/cli/commands/recipe-bundle/build_recipe_bundle.cc
+++ ecsact/cli/commands/recipe-bundle/build_recipe_bundle.cc
@@ -1,6 +1,6 @@
 #include "ecsact/cli/commands/recipe-bundle/build_recipe_bundle.hh"
 
-#include <archive.h>
+/* #include <archive.h> */
 
 auto ecsact::build_recipe_bundle::create( //
 	const build_recipe& recipe

90c3b699b57fcee0ed574ca1a10601f5afe9f538
Ezekiel Warren <ezekiel@seaube.com>
2024-05-09 09:34:10 -0700

    fix: dont canonicalize path

--- ecsact/cli/commands/recipe-bundle/build_recipe_bundle.cc
+++ ecsact/cli/commands/recipe-bundle/build_recipe_bundle.cc
@@ -203,7 +203,7 @@ auto ecsact::build_recipe_bundle::create( //
 				auto plugin_file_path =
 					cli::resolve_plugin_path(cli::resolve_plugin_path_options{
 						.plugin_arg = plugin,
-						.default_plugins_dir = cli::get_default_plugins_dir(),
+						.default_plugins_dir = recipe.base_directory(),
 						.additional_plugin_dirs = {recipe.base_directory()},
 					});
 

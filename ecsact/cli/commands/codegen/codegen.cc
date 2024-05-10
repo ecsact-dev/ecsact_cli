8b498c012028af964f6d3f120fda0ef7135586f8
Ezekiel Warren <ezekiel@seaube.com>
2024-05-09 17:14:17 -0700

    chore: improve plugin conflict error messages

--- ecsact/cli/commands/codegen/codegen.cc
+++ ecsact/cli/commands/codegen/codegen.cc
@@ -21,6 +21,8 @@ static void file_write_fn(const char* str, int32_t str_len) {
 
 auto ecsact::cli::codegen(codegen_options options) -> int {
 	auto plugins = std::vector<boost::dll::shared_library>{};
+	// key = plugin name, value = plugin path
+	auto plugin_names = std::unordered_map<std::string, std::string>{};
 
 	auto unload_plugins = [&plugins] {
 		for(auto& plugin : plugins) {
@@ -43,6 +45,16 @@ auto ecsact::cli::codegen(codegen_options options) -> int {
 			unload_plugins();
 			return 1;
 		}
+
+		auto plugin_name_fn = plugin.get<decltype(ecsact_codegen_plugin_name)>(
+			"ecsact_codegen_plugin_name"
+		);
+		std::string plugin_name = plugin_name_fn();
+		if(plugin_names.contains(plugin_name)) {
+			ecsact::cli::report_error("Multiple plugins with name '{}'", plugin_name);
+			unload_plugins();
+			return 1;
+		}
 	}
 
 	std::vector<ecsact_package_id> package_ids;
@@ -53,7 +65,8 @@ auto ecsact::cli::codegen(codegen_options options) -> int {
 		nullptr
 	);
 
-	auto output_paths = std::unordered_set<std::string>{};
+	// key is output path, value is plugin responsible for generating it
+	auto output_paths = std::unordered_map<std::string, std::string>{};
 	auto has_plugin_error = false;
 
 	for(auto& plugin : plugins) {
@@ -122,17 +135,21 @@ auto ecsact::cli::codegen(codegen_options options) -> int {
 
 			if(output_paths.contains(output_file_path.string())) {
 				has_plugin_error = true;
+				auto conflicting_plugin_name = output_paths[output_file_path.string()];
+
 				ecsact::cli::report_error(
-					"Plugin {} has conflicts with another plugin output file "
-					"{}\n",
+					"Plugin '{}' ({}) has conflicts with plugin '{}' output file "
+					"{}",
+					plugin_name,
 					plugin.location().filename().string(),
+					conflicting_plugin_name,
 					output_file_path.string()
 				);
 
 				continue;
 			}
 
-			output_paths.emplace(output_file_path.string());
+			output_paths.emplace(output_file_path.string(), plugin_name);
 			if(fs::exists(output_file_path)) {
 				fs::permissions(output_file_path, fs::perms::all);
 			}
